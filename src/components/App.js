1dc01e4252dbd96e7d83f15d14fba52beb23bc7c
hmyo2853 <hmyo2853@gmail.com>
2022-12-06 21:13:42 +0900

    #2.2

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import AppRouter from "../components/Router";
+import { authService } from "../fbase";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <div>
-      <AppRouter />
+      <AppRouter isLoggedIn={isLoggedIn} />
     </div>
   );
 }
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  const onChange = (e) => {
+    // change event target에서 name={}, value={}값이 email, password일 때 state를 변화시킨다.
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPwd(value);
+    }
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+          value={pwd}
+          onChange={onChange}
+        />
+        <input type="submit" value="log in" />
+      </form>
+      <div>
+        <button>Continue with Googlee</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
