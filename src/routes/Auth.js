3c20446a13c05207a536cf26a701edd0ef08e3cd
hmyo2853 <hmyo2853@gmail.com>
2022-12-06 22:11:54 +0900

    #2.6 google, github 로그인 구현 및 logout시 메인으로 돌려보내기

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { authService } from "../fbase";
+import { authService, firebaseInstance } from "../fbase";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
@@ -33,6 +33,20 @@ const Auth = () => {
   };
   // 버튼 state를 바꾸는 toggle handler
   const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (e) => {
+    const {
+      target: { name },
+    } = e;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -59,8 +73,12 @@ const Auth = () => {
         </span>
       </form>
       <div>
-        <button>Continue with Googlee</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,19 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { authService } from "../fbase";
 
-export default () => <span>Profile</span>;
+const Profile = () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    // 강제로 "/"로 돌려보내기
+    history.push("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log out</button>
+    </>
+  );
+};
+
+export default Profile;
